f5d54fd1a03fac6c4d1c7d756cbd1cccca6201b1
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-02-15 23:07:50 +0200

    Backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.json({ message: "Sign up route called" })
+}
+
+export const login = async (req, res) => {
+    res.json({ message: "Login route called" })
+}
+
+export const logout = async (req, res) => {
+    res.json({ message: "Logout route called" })
+}
