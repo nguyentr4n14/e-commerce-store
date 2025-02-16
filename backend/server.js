3d7f0f9bd494ed10c8e0a700f5d3ace2a97570d9
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-02-16 13:12:09 +0200

    Database setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ import dotenv from "dotenv"
 // Routes
 import authRoutes from './routes/auth.route.js'
 
+// Database
+import { connectDb } from './lib/db.js'
+
 dotenv.config()
 
 const app = express()
@@ -13,4 +16,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
+    connectDb()
 })
