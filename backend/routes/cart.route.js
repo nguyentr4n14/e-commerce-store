2da5b82bc4992d8309f3a42950949e168a6e9e35
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-02-18 17:14:08 +0200

    Coupon routes and controller added

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import { addToCart, getCartProducts, removeAllFromCart, updateQuantity } from '../controllers/cart.controller.js'
-import { protectRoute } from '../middleware/auth.middleware'
+import { protectRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
