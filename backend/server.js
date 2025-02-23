9db5d4a0d00ac34db8a2515808761247e07e9660
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-02-23 22:37:18 +0200

    Screenshot added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 // Routes
 import authRoutes from './routes/auth.route.js'
@@ -18,6 +19,8 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(express.json({ limit: "10mb" })) // Parse req.body
 app.use(cookieParser()) // Parse req.cookie
 
@@ -27,6 +30,14 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes)
 app.use("/api/analytics", analyticsRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
